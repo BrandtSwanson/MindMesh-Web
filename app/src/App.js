c9624bf5fb628be174d38b508614109548cd2f89
Brandt <brandtswan@gmail.com>
2023-10-01 16:22:39 -0500

    Connects to back end!

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+// Example API request in React using Axios
+import axios from 'axios';
+
+
 
 function App() {
+  const [data, setData] = useState({});
+  // Replace 'backend-url' with the actual URL of your Go backend
+  const backendUrl = 'http://localhost:8080'; // or your deployed backend URL
+
+  useEffect(() => {
+    // Make an API request
+    axios.get(`${backendUrl}/api/example`)
+      .then((response) => {
+        console.log(response.data);
+        const { sentence } = response.data;
+        setData(sentence);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!
+          The sentence is {JSON.stringify(data)}
         </p>
         <a
           className="App-link"
